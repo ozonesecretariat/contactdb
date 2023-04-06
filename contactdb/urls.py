5d0cc8ad72108ca0f928c5d010674324b40227e5
Alexandru Chirila <1538458+alexkiro@users.noreply.github.com>
2023-04-06 14:06:53 +0300

    Add admin password reset

--- contactdb/urls.py
+++ contactdb/urls.py
@@ -4,14 +4,36 @@ from django.contrib import admin
 from django.urls import include
 from django.urls import path
 from django.views.generic import RedirectView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+    path(
+        "admin/password_reset/",
+        auth_views.PasswordResetView.as_view(),
+        name="admin_password_reset",
+    ),
+    path(
+        "admin/password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "admin/reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "admin/reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
     path("admin/", admin.site.urls),
     path("", RedirectView.as_view(pattern_name="admin:index")),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     try:
         import debug_toolbar
     except ImportError:
