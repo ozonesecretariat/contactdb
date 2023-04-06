44b3ca57ddab01f5d512d163130d7c19af43ce43
Alexandru Chirila <1538458+alexkiro@users.noreply.github.com>
2023-04-06 13:22:32 +0300

    Add django-debug-toolbar

--- contactdb/settings.py
+++ contactdb/settings.py
@@ -197,3 +197,38 @@ DEFAULT_FROM_EMAIL = env.str("DEFAULT_FROM_EMAIL", default="") or (
 ENVIRONMENT_NAME = env.str("ENVIRONMENT_NAME", default="")
 ENVIRONMENT_COLOR = env.str("ENVIRONMENT_COLOR", default="")
 ENVIRONMENT_TEXT_COLOR = env.str("ENVIRONMENT_TEXT_COLOR", default="#ffffff")
+
+
+if DEBUG:
+    DJANGO_DEBUG_TOOLBAR = env.bool("DJANGO_DEBUG_TOOLBAR", default=True)
+
+    INTERNAL_IPS = ["127.0.0.1"]
+
+    if DJANGO_DEBUG_TOOLBAR:
+        INSTALLED_APPS += ["debug_toolbar"]
+
+    if DEBUG and DJANGO_DEBUG_TOOLBAR:
+        import socket
+
+        def show_toolbar(request):
+            return DEBUG and (
+                request.META.get("REMOTE_ADDR") in INTERNAL_IPS
+                or request.META.get("HTTP_X_REAL_IP") in INTERNAL_IPS
+            )
+
+        hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+        INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
+        MIDDLEWARE.insert(2, "debug_toolbar.middleware.DebugToolbarMiddleware")
+        DEBUG_TOOLBAR_CONFIG = {
+            "SHOW_COLLAPSED": True,
+            "SHOW_TOOLBAR_CALLBACK": f"{__name__}.show_toolbar",
+        }
+
+    AUTH_PASSWORD_VALIDATORS = [
+        {
+            "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+            "OPTIONS": {
+                "min_length": 4,
+            },
+        },
+    ]
--- contactdb/urls.py
+++ contactdb/urls.py
@@ -1,4 +1,7 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.urls import include
 from django.urls import path
 from django.views.generic import RedirectView
 
@@ -6,3 +9,12 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", RedirectView.as_view(pattern_name="admin:index")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    try:
+        import debug_toolbar
+    except ImportError:
+        pass
+    else:
+        urlpatterns.append(path("__debug__/", include(debug_toolbar.urls)))
