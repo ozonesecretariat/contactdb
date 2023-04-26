363a2b71bb9eee0fc0effce674dcdae643a33687
Andrei Niculescu <63136724+andynick990@users.noreply.github.com>
2023-04-26 12:17:41 +0300

    Add contacts models (#3)
    * Add contacts models
    
    * Fix black issues

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django_task.admin import TaskAdmin
 
-from core.models import SendMailTask
+from core.models import SendMailTask, Organization, RegistrationStatus, Record
 
 
 @admin.register(SendMailTask)
@@ -24,3 +24,38 @@ class SendMailTaskAdmin(TaskAdmin):
         fields = super().get_list_display(request)
         fields.remove("log_link_display")
         return fields
+
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    search_fields = [
+        "organization_id",
+        "name",
+        "acronym",
+        "organization_type_id",
+        "government",
+        "government_name",
+        "country",
+        "country_name",
+    ]
+    list_filter = ["organization_type"]
+
+
+@admin.register(RegistrationStatus)
+class RegistrationStatusAdmin(admin.ModelAdmin):
+    search_fields = ["event_id", "status", "contact__first_name", "contact__last_name"]
+    list_display = ("contact", "event_id")
+    list_filter = ["is_funded", "status", "role"]
+
+
+@admin.register(Record)
+class RecordAdmin(admin.ModelAdmin):
+    search_fields = [
+        "contact_id",
+        "first_name",
+        "last_name",
+        "designation",
+        "department",
+        "organization__name",
+    ]
+    list_filter = ["is_in_mailing_list", "is_use_organization_address"]
--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django_task.models import TaskRQ
+from django.contrib.postgres.fields import ArrayField
 
 
 class SendMailTask(TaskRQ):
@@ -30,3 +31,66 @@ class SendMailTask(TaskRQ):
         from .jobs import SendMailJob
 
         return SendMailJob
+
+
+class Organization(models.Model):
+    organization_id = models.CharField(max_length=250, unique=True)
+    name = models.CharField(max_length=250)
+    acronym = models.CharField(blank=True, max_length=30)
+    organization_type_id = models.CharField(max_length=250)
+    organization_type = models.CharField(max_length=250)
+    government = models.CharField(max_length=30, blank=True)
+    government_name = models.CharField(max_length=250, blank=True)
+    country = models.CharField(max_length=30, blank=True)
+    country_name = models.CharField(max_length=250, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Record(models.Model):
+    organization = models.ForeignKey(Organization, on_delete=models.SET_NULL, null=True)
+    contact_id = models.CharField(max_length=250, unique=True)
+    title = models.CharField(max_length=30, blank=True)
+    first_name = models.CharField(max_length=250, default="", blank=True)
+    last_name = models.CharField(max_length=250, default="", blank=True)
+    designation = models.CharField(max_length=250, default="", blank=True)
+    department = models.CharField(max_length=250, default="", blank=True)
+    affiliation = models.CharField(max_length=250, default="", blank=True)
+    phones = ArrayField(base_field=models.TextField(), blank=True)
+    mobiles = ArrayField(base_field=models.TextField(), blank=True)
+    faxes = ArrayField(base_field=models.TextField(), blank=True)
+    emails = ArrayField(base_field=models.TextField(), blank=True)
+    email_ccs = ArrayField(base_field=models.TextField(), blank=True)
+    notes = models.TextField(default="", blank=True)
+    is_in_mailing_list = models.BooleanField()
+    is_use_organization_address = models.BooleanField()
+    address = models.CharField(max_length=250, default="", blank=True)
+    city = models.CharField(max_length=250, default="", blank=True)
+    state = models.CharField(max_length=250, default="", blank=True)
+    country = models.CharField(max_length=250, default="", blank=True)
+    postal_code = models.CharField(max_length=250, default="", blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+
+class RegistrationStatus(models.Model):
+    contact = models.ForeignKey(Record, on_delete=models.CASCADE)
+    event_id = models.CharField(max_length=150)
+    code = models.CharField(max_length=100)
+    status = models.IntegerField()
+    date = models.DateTimeField()
+    is_funded = models.BooleanField()
+    role = models.IntegerField(null=True)
+    priority_pass_code = models.CharField(max_length=150, blank=True)
+    tags = ArrayField(base_field=models.TextField(), blank=True)
+
+    def __str__(self):
+        return self.event_id
+
+    class Meta:
+        verbose_name_plural = "registration statuses"
