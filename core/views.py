fe1168711c75f97d2d664a92dc4a8771ede805b7
Andrei Niculescu <andy_nick98@yahoo.com>
2023-05-08 15:44:44 +0300

    Add Records list page

--- core/views.py
+++ core/views.py
@@ -1,6 +1,28 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
+from django_tables2 import SingleTableMixin
+from django_filters.views import FilterView
+
+from core.models import Record
+from core.tables import RecordTable
+from core.filters import RecordFilter
+
 
 class HomepageView(LoginRequiredMixin, TemplateView):
     template_name = "home_page.html"
+
+
+class ContactListView(LoginRequiredMixin, SingleTableMixin, FilterView):
+    table_class = RecordTable
+    queryset = Record.objects.all()
+    filterset_class = RecordFilter
+    paginate_by = 30
+
+    def get_template_names(self):
+        if self.request.htmx:
+            template_name = "records_list_page_partial.html"
+        else:
+            template_name = "records_list_page.html"
+
+        return template_name
