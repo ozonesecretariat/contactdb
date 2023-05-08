76a2e713eec82dd2687e44ceb35977fd5c85476c
Andrei Niculescu <63136724+andynick990@users.noreply.github.com>
2023-05-08 15:49:55 +0300

    Add Records list page (#8)
    * Add Records list page
    
    * Add newline

---
+++ core/filters.py
@@ -0,0 +1,70 @@
+import django_filters
+from django.db.models import Q
+from core.models import Record, Organization
+from django import forms
+
+MAILING_LIST = (
+    (True, "Yes"),
+    (False, "No"),
+)
+
+
+class RecordFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(
+        method="name_search", widget=forms.TextInput(attrs={"class": "form-control"})
+    )
+    department = django_filters.CharFilter(
+        lookup_expr="icontains", widget=forms.TextInput(attrs={"class": "form-control"})
+    )
+    designation = django_filters.CharFilter(
+        lookup_expr="icontains", widget=forms.TextInput(attrs={"class": "form-control"})
+    )
+    is_in_mailing_list = django_filters.ChoiceFilter(
+        choices=MAILING_LIST, widget=forms.Select(attrs={"class": "form-select"})
+    )
+    is_use_organization_address = django_filters.ChoiceFilter(
+        choices=MAILING_LIST, widget=forms.Select(attrs={"class": "form-select"})
+    )
+    organization = django_filters.ChoiceFilter(
+        choices=Organization.objects.all().values_list("id", "name"),
+        widget=forms.Select(attrs={"class": "form-select"}),
+    )
+    emails = django_filters.CharFilter(
+        method="email_search", widget=forms.TextInput(attrs={"class": "form-control"})
+    )
+    phones_faxes = django_filters.CharFilter(
+        method="phone_faxes_search",
+        widget=forms.TextInput(attrs={"class": "form-control"}),
+    )
+
+    class Meta:
+        model = Record
+        fields = [
+            "name",
+            "department",
+            "designation",
+            "is_in_mailing_list",
+            "is_use_organization_address",
+            "organization",
+            "emails",
+            "phones_faxes",
+        ]
+
+    def name_search(self, queryset, name, value):
+        for term in value.split():
+            queryset = queryset.filter(
+                Q(first_name__icontains=term) | Q(last_name__icontains=term)
+            )
+        return queryset
+
+    def email_search(self, queryset, name, value):
+        return queryset.filter(
+            Q(emails__icontains=value) | Q(email_ccs__icontains=value)
+        )
+
+    def phone_faxes_search(self, queryset, name, value):
+        return queryset.filter(
+            Q(phones__icontains=value)
+            | Q(mobiles__icontains=value)
+            | Q(faxes__icontains=value)
+        )
