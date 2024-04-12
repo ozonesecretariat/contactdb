eb7be199c8a36c4838d830190196d69525b999bd
Alexandru Chirila <1538458+alexkiro@users.noreply.github.com>
2024-04-12 11:11:24 +0300

    Use contacts ids to identify unique contacts

--- events/parsers.py
+++ events/parsers.py
@@ -1,7 +1,7 @@
 import logging
+import re
 from datetime import datetime
 
-from django.db.models.functions import Trim, Lower
 from django.utils.timezone import make_aware
 
 from core.models import (
@@ -24,6 +24,25 @@ from common.utils import check_diff
 class KronosParser:
     def __init__(self, task):
         self.task = task
+
+    def parse_email_list(self, email_list):
+        if isinstance(email_list, str):
+            email_list = [email_list]
+
+        result = set()
+        for item in email_list:
+            result.update(re.split(r"[;,]", item))
+
+        return list(result)
+
+    def parse_date(self, value):
+        try:
+            return datetime.strptime(value, "%Y-%m-%d").date()
+        except (TypeError, ValueError):
+            return None
+
+    def parse_datetime(self, value):
+        return make_aware(datetime.strptime(value, "%Y-%m-%dT%H:%M:%SZ"))
 
     def get_country(self, code: str, name=None):
         if not code:
@@ -84,14 +103,10 @@ class KronosEventsParser(KronosParser):
 
             d = {
                 "event_id": event_dict.get("eventId"),
-                "title": event_dict.get("title"),
+                "title": event_dict.get("title", "").strip(),
                 "code": event_dict.get("code"),
-                "start_date": make_aware(
-                    datetime.strptime(event_dict.get("startDate"), "%Y-%m-%dT%H:%M:%SZ")
-                ),
-                "end_date": make_aware(
-                    datetime.strptime(event_dict.get("endDate"), "%Y-%m-%dT%H:%M:%SZ")
-                ),
+                "start_date": self.parse_datetime(event_dict["startDate"]),
+                "end_date": self.parse_datetime(event_dict["endDate"]),
                 "venue_country": self.get_country(event_dict.get("venueCountry")),
                 "venue_city": event_dict.get("venueCity"),
                 "dates": event_dict.get("dates"),
@@ -125,24 +140,48 @@ class KronosEventsParser(KronosParser):
 
 
 class KronosParticipantsParser(KronosParser):
+    field_mapping = {
+        "organization": "organization",
+        "title": "title",
+        "firstName": "first_name",
+        "lastName": "last_name",
+        "designation": "designation",
+        "department": "department",
+        "affiliation": "affiliation",
+        "phones": "phones",
+        "mobiles": "mobiles",
+        "faxes": "faxes",
+        "emails": "emails",
+        "emailCcs": "email_ccs",
+        "notes": "notes",
+        "isInMailingList": "is_in_mailing_list",
+        "isUseOrganizationAddress": "is_use_organization_address",
+        "address": "address",
+        "city": "city",
+        "state": "state",
+        "country": "country",
+        "postalCode": "postal_code",
+        "dateOfBirth": "birth_date",
+    }
+
     def create_registrations(self, contact_dict, contact):
         for registration in contact_dict["registrationStatuses"]:
             if registration is None:
                 continue
 
             event = Event.objects.filter(event_id=registration["eventId"]).first()
-            status_kronos_id = registration["status"]
+            status_kronos_enum = registration["status"]
             status = RegistrationStatus.objects.get_or_create(
-                kronos_id=status_kronos_id,
+                kronos_value=status_kronos_enum,
                 defaults={
-                    "name": status_kronos_id,
+                    "name": status_kronos_enum,
                 },
             )[0]
-            role_kronos_id = registration.get("role")
+            role_kronos_enum = registration.get("role")
             role = RegistrationRole.objects.get_or_create(
-                kronos_id=role_kronos_id,
+                kronos_value=role_kronos_enum,
                 defaults={
-                    "name": role_kronos_id,
+                    "name": role_kronos_enum,
                 },
             )[0]
 
@@ -173,83 +212,49 @@ class KronosParticipantsParser(KronosParser):
 
     def parse_contact_list(self, contact_list):
         for contact_dict in contact_list:
-            organization = self.get_org(contact_dict.get("organization", {}))
-
-            contact = (
-                Contact.objects.annotate(
-                    trim_first_name=Lower(Trim("first_name")),
-                    trim_last_name=Lower(Trim("last_name")),
-                )
-                .filter(
-                    emails=contact_dict.get("emails"),
-                    trim_first_name=contact_dict["firstName"].strip().lower(),
-                    trim_last_name=contact_dict["lastName"].strip().lower(),
-                )
-                .first()
+            contact_id = contact_dict["contactId"]
+            contact_dict["dateOfBirth"] = self.parse_date(
+                contact_dict.get("dateOfBirth")
             )
-            birth_date_string = contact_dict.get("dateOfBirth", "")
-            if birth_date_string:
-                birth_date = datetime.strptime(birth_date_string, "%Y-%m-%d").date()
-            else:
-                birth_date = None
-
+            contact_dict["country"] = self.get_country(
+                contact_dict.get("country"), contact_dict.get("countryName")
+            )
+            contact_dict["organization"] = self.get_org(
+                contact_dict.get("organization", {})
+            )
             contact_defaults = {
-                "contact_id": contact_dict.get("contactId"),
-                "organization": organization,
-                "title": contact_dict.get("title"),
-                "first_name": contact_dict.get("firstName"),
-                "last_name": contact_dict.get("lastName"),
-                "designation": contact_dict.get("designation"),
-                "department": contact_dict.get("department"),
-                "affiliation": contact_dict.get("affiliation"),
-                "phones": contact_dict.get("phones"),
-                "mobiles": contact_dict.get("mobiles"),
-                "faxes": contact_dict.get("faxes"),
-                "emails": contact_dict.get("emails"),
-                "email_ccs": contact_dict.get("emailCcs"),
-                "notes": contact_dict.get("notes"),
-                "is_in_mailing_list": contact_dict.get("isInMailingList"),
-                "is_use_organization_address": contact_dict.get(
-                    "isUseOrganizationAddress"
-                ),
-                "address": contact_dict.get("address", ""),
-                "city": contact_dict.get("city", ""),
-                "state": contact_dict.get("state", ""),
-                "country": self.get_country(
-                    contact_dict.get("country"),
-                    contact_dict.get("countryName"),
-                ),
-                "postal_code": contact_dict.get("postalCode", ""),
-                "birth_date": birth_date,
+                model_attr: contact_dict.get(kronos_attr)
+                for kronos_attr, model_attr in self.field_mapping.items()
             }
+
+            contact = Contact.objects.filter(contact_id=contact_id).first()
             if contact:
                 if check_diff(contact, contact_defaults):
                     self.task.log(
                         logging.INFO,
-                        f"A contact with the same name and emails as {contact_dict.get('contactId')} is already in database;"
-                        f" adding it to the temporary table for conflict resolution",
+                        "A contact with the ID as %s is already in database; "
+                        "adding it to the temporary table for conflict resolution: %s",
+                        contact,
+                        contact_id,
                     )
-                    contact_defaults["existing_contact"] = contact
-                    conflict, created = ResolveConflict.objects.get_or_create(
-                        contact_id=contact_dict.get("contactId"),
-                        emails=contact_dict.get("emails"),
-                        defaults=contact_defaults,
+                    ResolveConflict.objects.create(
+                        existing_contact=contact, **contact_defaults
                     )
-                    self.task.conflicts_nr += created
+                    self.task.conflicts_nr += 1
                 else:
                     self.task.log(
                         logging.INFO,
-                        f"A contact with the same data as {contact_dict.get('contactId')} is already in database;",
+                        "A contact with the same data as %s is already in database: %s",
+                        contact,
+                        contact_id,
                     )
                     self.task.skipped_nr += 1
             else:
-                contact, created = Contact.objects.get_or_create(
-                    contact_id=contact_dict.get("contactId"),
-                    emails=contact_dict.get("emails"),
-                    defaults=contact_defaults,
+                contact = Contact.objects.create(
+                    contact_id=contact_id, **contact_defaults
                 )
-                self.task.contacts_nr += created
-                self.task.skipped_nr += not created
-                if created:
-                    self.task.log(logging.INFO, "Created contact: %s", contact)
+                self.task.contacts_nr += 1
+                self.task.log(
+                    logging.INFO, "Created contact %s: %s", contact, contact_id
+                )
             self.create_registrations(contact_dict, contact)
