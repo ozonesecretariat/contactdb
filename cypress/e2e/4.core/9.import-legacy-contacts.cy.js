91abedd0ad2cd637a92ad8eb89da1cbf2cea60df
Alexandru Chirila <1538458+alexkiro@users.noreply.github.com>
2024-05-15 10:22:13 +0300

    Add test for importing legacy contacts

--- cypress/e2e/4.core/9.import-legacy-contacts.cy.js
+++ cypress/e2e/4.core/9.import-legacy-contacts.cy.js
@@ -6,4 +6,36 @@ describe("Check import focal points", () => {
       searchValue: "4a0665ef-d79f-444e-84d9-35f26fbef7fc",
     });
   });
+  it.only("Check import legacy contacts", () => {
+    cy.loginAdmin();
+    cy.addModel("Import legacy contacts task", {
+      json_file: "fixtures/test/files/legacy-contacts.json",
+    });
+    cy.get("#result_list tbody tr:first-of-type .field-status_display").contains("SUCCESS");
+    cy.get("#result_list tbody tr:first-of-type th a").click();
+    cy.contains("Contacts created=2");
+
+    // Check some of the imported fields
+    cy.performSearch({
+      modelName: "Contacts",
+      searchValue: "jane.legacy-test@example.com",
+      filters: { groups__id__exact: "Legacy contacts" },
+    });
+    cy.get("#result_list tbody tr:first-of-type th a").click();
+    cy.contains("Ms. Jane Doe");
+    cy.contains("Guyana (GY)");
+    cy.get("[name=emails]").eq(0).should("have.value", "jane.legacy-test@example.com");
+    cy.get("[name=emails]").eq(1).should("have.value", "jane2.legacy-test@example.com");
+    cy.get("[name=phones]").eq(0).should("have.value", "(+00) 123 456 789");
+    cy.get("[name=designation]").should("have.value", "Officer");
+
+    // Remove imported data
+    cy.triggerAction({
+      modelName: "Contacts",
+      action: "Delete selected contacts",
+      searchValue: "legacy-test@example.com",
+      filters: { groups__id__exact: "Legacy contacts" },
+    });
+    cy.get("[type=submit]").contains("Yes, I’m sure").click();
+  });
 });
