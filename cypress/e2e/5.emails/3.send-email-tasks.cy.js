1955d1b6efcc18589d2d90bf43fdef38be9cda63
Alexandru Chirila <1538458+alexkiro@users.noreply.github.com>
2024-06-19 12:56:01 +0300

    Add tests checking filtering send email task logs

--- cypress/e2e/5.emails/3.send-email-tasks.cy.js
+++ cypress/e2e/5.emails/3.send-email-tasks.cy.js
@@ -28,4 +28,47 @@ describe("Check", () => {
     cy.get("a").contains("Atlas Drake (Deep Space Exploration Agency, Jamaica)").click();
     cy.contains("Change contact");
   });
+  it("Check filter Any contact", () => {
+    cy.loginAdmin();
+    cy.performSearch({
+      modelName: "Send email tasks",
+      filters: {
+        any_contact: "Axel-Elio Neutron",
+      },
+    });
+    cy.contains("47 results");
+  });
+  it("Check filter To contact", () => {
+    cy.loginAdmin();
+    cy.performSearch({
+      modelName: "Send email tasks",
+      filters: {
+        email: "Test email with Cc and Bcc",
+        contact: "Rigel Rift",
+      },
+    });
+    cy.contains("1 result");
+  });
+  it("Check filter Cc contact", () => {
+    cy.loginAdmin();
+    cy.performSearch({
+      modelName: "Send email tasks",
+      filters: {
+        email: "Test email with Cc and Bcc",
+        cc_contacts: "🐉",
+      },
+    });
+    cy.contains("46 results");
+  });
+  it("Check filter Bcc contact", () => {
+    cy.loginAdmin();
+    cy.performSearch({
+      modelName: "Send email tasks",
+      filters: {
+        email: "Test email with Cc and Bcc",
+        bcc_contacts: "Ezra Spectrum",
+      },
+    });
+    cy.contains("46 results");
+  });
 });
