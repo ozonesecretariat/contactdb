15138603f186a0da332a9a1405b645049dc3b478
Andrei Meliș <andrei.melis@eaudeweb.ro>
2025-05-29 19:17:02 +0300

    Rebase and rename migration

--- core/migrations/0017_remove_contact_is_in_mailing_list_and_more.py
+++ core/migrations/0017_remove_contact_is_in_mailing_list_and_more.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("core", "0015_alter_contact_organization"),
+        ("core", "0016_alter_organization_organization_type"),
     ]
 
     operations = [
