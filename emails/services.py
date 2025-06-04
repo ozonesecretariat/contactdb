3f6480e852d3eb2a9f2498246842711b25f3f1e3
Sorin Stelian <sorin.stelian@eaudeweb.ro>
2025-06-04 15:14:57 +0300

    Improving prefetch behavior for organizations

--- emails/services.py
+++ emails/services.py
@@ -1,23 +1,44 @@
+from django.db import models
+
+from core.models import Organization
+
+
 def get_organization_recipients(org_types):
     """
-    Get recipients per organization for invitation emails, using the organization's
+    Get recipients per organization for *invitation* emails, using the organization's
     primary and secondary contacts.
 
     This is useful for sending emails to users that need to register for an
     upcoming event.
     """
+    org_types = org_types.prefetch_related(
+        models.Prefetch(
+            "organizations",
+            queryset=Organization.objects.filter(
+                include_in_invitation=True
+            ).prefetch_related(
+                "primary_contacts",
+                "secondary_contacts",
+                "government",
+            ),
+        )
+    )
+
     org_recipients = {}
     for org_type in org_types:
-        for org in org_type.organizations.filter(
-            include_in_invitation=True
-        ).prefetch_related("primary_contacts", "secondary_contacts"):
+        for org in org_type.organizations.filter(include_in_invitation=True).all():
             primary = set(org.primary_contacts.all())
             secondary = set(org.secondary_contacts.all())
+
             if org_type.acronym == "GOV":
                 # If it's a GOV, include all invite-able orgs from that country
-                for related_organization in org.get_related_invite_organizations():
+                related_orgs = Organization.objects.filter(
+                    government=org.government, include_in_invitation=True
+                ).prefetch_related("primary_contacts", "secondary_contacts")
+                for related_organization in related_orgs:
                     primary |= set(related_organization.primary_contacts.all())
                     secondary |= set(related_organization.secondary_contacts.all())
+
             if primary or secondary:
                 org_recipients[org] = {
                     "to": primary,
