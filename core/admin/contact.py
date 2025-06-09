9777a45192af25ea289f4ffaee1379108fd09387
Madalina Boboc <madaeboboc@gmail.com>
2025-06-09 13:20:05 +0300

    Add organization to autocomplete field

--- core/admin/contact.py
+++ core/admin/contact.py
@@ -87,7 +87,7 @@ class ContactMembershipInline(admin.StackedInline):
 class ContactRegistrationsInline(admin.StackedInline):
     extra = 0
     model = Registration
-    autocomplete_fields = ("event", "status", "role", "tags")
+    autocomplete_fields = ("event", "status", "role", "tags", "organization")
     classes = ["collapse"]
 
     fieldsets = (
