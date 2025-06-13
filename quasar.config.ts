0eeb5ca2dadc5f79802da61dc3eaff16d3ad0dd6
alexkiro <1538458+alexkiro@users.noreply.github.com>
2025-06-13 10:18:41 +0300

    Add font and remove roboto

--- quasar.config.ts
+++ quasar.config.ts
@@ -166,8 +166,7 @@ export default defineConfig((/* ctx */) => ({
     // 'themify',
     // 'line-awesome',
     // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
-
-    "roboto-font", // optional, you are not bound to it
+    // "roboto-font", // optional, you are not bound to it
     "material-icons", // optional, you are not bound to it
   ],
 
