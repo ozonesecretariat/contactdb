f4555eb5603609e03a44dfb00931baca068a4ce0
Madalina Boboc <41815939+mboboc@users.noreply.github.com>
2025-07-18 17:07:32 +0300

    Add management command for importing contacts, refs #30861 (#120)
    * Management command for importing contacts, refs #30861 (squashed)
    * Merge updates
    * Update logic for title_localized
    
    ---------
    
    Co-authored-by: Andrei Meliș <andrei.melis@eaudeweb.ro>

--- core/admin/resolve_conflict.py
+++ core/admin/resolve_conflict.py
@@ -1,13 +1,17 @@
 from copy import copy
+from itertools import chain
 
 from admin_auto_filters.filters import AutocompleteFilterFactory
 from django.contrib import admin, messages
+from django.db import transaction
+from django.db.models import Q
 from django.utils.html import format_html
 
 from common.urls import reverse
 from core.admin import ContactAdminBase
 from core.admin.contact_base import MERGE_FROM_PARAM
-from core.models import ResolveConflict
+from core.models import Contact, ResolveConflict
+from core.parsers import ContactParser
 
 
 @admin.register(ResolveConflict)
@@ -53,7 +57,7 @@ class ResolveConflictAdmin(ContactAdminBase):
         "existing_contact__organization__country",
         "existing_contact__organization__government",
     )
-    actions = ("accept_new_data",)
+    actions = ("accept_new_data", "keep_both_contacts")
 
     @admin.action(
         description="Accept new data for selected conflicts", permissions=["delete"]
@@ -68,6 +72,88 @@ class ResolveConflictAdmin(ContactAdminBase):
             f"{len(new_contacts)} conflicts resolved",
             messages.SUCCESS,
         )
+
+    @admin.action(
+        description="Keep both contacts",
+        permissions=["delete"],
+    )
+    def keep_both_contacts(self, request, queryset):
+        """
+        Convert conflicts to contacts. If a contact has multiple
+        contact_ids (e.g. it's a merged Kronos contact), all contact_ids
+        will be reimported from Kronos.
+        """
+
+        def create_contacts_from_conflicts(conflict_queryset) -> int:
+            conflicts_dict = list(conflict_queryset.values())
+            conflicts_ids = []
+
+            with transaction.atomic():
+                for conflict in conflicts_dict:
+                    conflict.pop("existing_contact_id", None)
+                    conflict_id = conflict.pop("id", None)
+                    conflicts_ids.append(conflict_id)
+
+                    Contact.objects.create(**conflict)
+
+                ResolveConflict.objects.filter(pk__in=conflicts_ids).delete()
+            return len(conflicts_ids)
+
+        # Handle conflicts with no contact_ids
+        no_contact_ids_qs = queryset.filter(
+            Q(existing_contact__contact_ids__isnull=True)
+            | Q(existing_contact__contact_ids=[])
+        ).values()
+        count_created = create_contacts_from_conflicts(no_contact_ids_qs)
+
+        if count_created:
+            self.message_user(
+                request,
+                f"Created {count_created} contacts from conflicts.",
+                messages.SUCCESS,
+            )
+
+        # Reimport contacts with contact_ids from Kronos
+        kronos_contacts = (
+            Contact.objects.filter(
+                conflicting_contacts__in=queryset, contact_ids__isnull=False
+            )
+            .exclude(contact_ids=[])
+            .distinct()
+        )
+
+        if kronos_contacts.exists():
+            kronos_ids = set(
+                chain.from_iterable(
+                    kronos_contacts.values_list("contact_ids", flat=True)
+                )
+            )
+
+            # Convert all related conflicts to contacts before
+            # reimporting
+            related_conflicts = ResolveConflict.objects.filter(
+                existing_contact__in=kronos_contacts
+            )
+            count_created = create_contacts_from_conflicts(related_conflicts)
+
+            if count_created:
+                self.message_user(
+                    request,
+                    f"Created {count_created} contacts from conflicts.",
+                    messages.SUCCESS,
+                )
+
+            contact_parser = ContactParser()
+            imported_contacts = contact_parser.import_contacts_with_registrations(
+                kronos_ids
+            )
+
+            if imported_contacts:
+                self.message_user(
+                    request,
+                    f"Reimported {len(imported_contacts)} contacts with registrations from Kronos.",
+                    messages.SUCCESS,
+                )
 
     @staticmethod
     def save_incoming_data(incoming_contact):
---
+++ core/tests/test_import_contacts_from_kronos.py
@@ -0,0 +1,215 @@
+import datetime
+from copy import deepcopy
+from unittest.mock import patch
+
+from django.test import TestCase
+
+from core.models import (
+    Contact,
+    Country,
+    Organization,
+    OrganizationType,
+)
+from core.parsers import ContactParser
+from events.models import Event, RegistrationRole
+
+
+class TestImportContactFromKronos(TestCase):
+    fixtures = [
+        "initial/region",
+        "initial/subregion",
+        "initial/country",
+        "initial/organizationtype",
+        "initial/registrationrole",
+        "initial/registrationtag",
+    ]
+
+    def setUp(self):
+        self.event = Event.objects.create(
+            event_id="eventid",
+            title="Quantum Quest: A Science Adventure Symposium",
+            code="POM-2023",
+            start_date="2023-01-01T00:00:00Z",
+            end_date="2023-01-05T00:00:00Z",
+            dates="1 - 5 January 2023",
+        )
+
+        self.organization = Organization.objects.create(
+            name="Literary Society",
+            organization_id="orgid",
+            acronym="LS",
+            country=Country.objects.get(code="GB"),
+            government=Country.objects.get(code="GB"),
+            organization_type=OrganizationType.objects.get(acronym="GOV"),
+        )
+
+        self.fake_contact = {
+            "contactId": "contactid",
+            "organizationId": "orgid",
+            "organizationType": "GOV",
+            "title": "Mme.",
+            "firstName": "Jane",
+            "lastName": "Eyre",
+            "designation": "Expert",
+            "department": "Environment and Energy",
+            "phones": ["+4999999999"],
+            "mobiles": ["+4999999999"],
+            "faxes": ["+4999999999"],
+            "emails": ["jane@book.com"],
+            "emailCcs": ["jane@book.net"],
+            "notes": "I would always rather be happy than dignified. ##F, ##A, ##S",
+            "isInMailingList": False,
+            "isUseOrganizationAddress": True,
+            "address": "Thornfield Hall, main road",
+            "city": "Thornfield Hall",
+            "state": "Hallshire",
+            "country": "gb",
+            "postalCode": "01234",
+            "createdOn": "2015-05-08T12:03:31.019Z",
+            "createdBy": "Charlotte Brontë",
+            "updatedOn": "2025-06-08T14:03:21.019Z",
+            "updatedBy": "Charlotte Brontë",
+            "dateOfBirth": "1847-10-19",
+        }
+
+        self.fake_registration = [
+            {
+                "registrationId": "regid",
+                "eventId": "eventid",
+                "event": {
+                    "eventId": "eventid",
+                    "code": "POM-2023",
+                    "title": "First Meeting of the Ladies in Blue Pants",
+                    "startDate": "2023-01-01T00:00:00Z",
+                    "endDate": "2023-01-05T00:00:00Z",
+                    "venueCountry": "ec",
+                    "venueCity": "Quito",
+                    "dates": "1 - 5 January 2023",
+                    "isOpenForRegistration": True,
+                },
+                "nominatedOn": "2022-12-01T10:00:00Z",
+                "nominatedBy": "Charlotte Brontë",
+                "accreditedOn": "2022-12-15T10:00:00Z",
+                "accreditedBy": "Charlotte Brontë",
+                "registeredOn": "2023-01-01T10:00:00Z",
+                "registeredBy": "Charlotte Brontë",
+                "isFunded": False,
+                "isVisaRequired": False,
+                "position": 5,
+                "role": 1,
+                "status": 4,
+                "dynamicTags": [
+                    "EVENT:00000000000000000004",
+                    "CONTACT:00000000000000000001",
+                    "TYPE:GOV",
+                    "PARTY:TRUE",
+                    "PARTY_TO:XXVII1",
+                    "REGION:AFRICA",
+                    "GROUP:AFRICA",
+                ],
+                "priorityPass": "LETMEIN",
+                "priorityPassCode": "LETMEIN",
+                "priorityPassGeneratedOn": "2022-12-20T10:00:00Z",
+                "priorityPassSentOn": "2022-12-20T10:00:00Z",
+                "priorityPassSentTo": "jane@book.com",
+                "priorityPassUrl": "www.example.com/priority_pass.pdf",
+                "passportCount": 0,
+                "contactId": "contactid",
+                "organizationId": "orgid",
+                "organizationType": "GOV",
+                "title": "Mr.",
+                "firstName": "Jane",
+                "lastName": "Eyre",
+                "designation": "Expert",
+                "department": "Environment and Energy",
+                "phones": ["+4999999999"],
+                "mobiles": ["+4999999999"],
+                "faxes": ["+4999999999"],
+                "emails": ["jane@book.com"],
+                "emailCcs": ["jane@book.com"],
+                "notes": "I would always rather be happy than dignified. ##F, ##A, ##S",
+                "isInMailingList": False,
+                "isUseOrganizationAddress": True,
+                "address": "Thornfield Hall, main road",
+                "city": "Thornfield Hall",
+                "state": "Hallshire",
+                "country": "gb",
+                "postalCode": "01234",
+                "createdOn": "2015-05-08T12:03:31.019Z",
+                "createdBy": "Charlotte Brontë",
+                "updatedOn": "2025-06-08T14:03:21.019Z",
+                "updatedBy": "Charlotte Brontë",
+            }
+        ]
+
+        self.mock_client = patch("core.parsers.KronosClient").start()
+        self.mock_client.return_value.get_contact_data.side_effect = (
+            lambda *args: deepcopy(self.fake_contact)
+        )
+
+        self.mock_client.return_value.get_registrations_data.side_effect = (
+            lambda *args: deepcopy(self.fake_registration)
+        )
+
+    def tearDown(self):
+        patch.stopall()
+
+    def test_import_contact_from_kronos(self):
+        parser = ContactParser()
+
+        parser.import_contacts_with_registrations(
+            [self.fake_contact["contactId"]],
+        )
+
+        self.assertEqual(Contact.objects.count(), 1)
+        contact = Contact.objects.first()
+
+        self.assertEqual(contact.contact_ids, ["contactid"])
+        self.assertIsNotNone(contact.organization)
+        self.assertEqual(contact.organization.name, "Literary Society")
+        self.assertEqual(contact.organization.organization_id, "orgid")
+        self.assertIsNotNone(contact.country)
+
+        self.assertEqual(contact.title, "Ms.")
+        self.assertEqual(contact.title_localized, "Mme.")
+        self.assertEqual(contact.first_name, "Jane")
+        self.assertEqual(contact.last_name, "Eyre")
+        self.assertEqual(contact.designation, "Expert")
+        self.assertEqual(contact.department, "Environment and Energy")
+        self.assertEqual(contact.phones, ["+4999999999"])
+        self.assertEqual(contact.mobiles, ["+4999999999"])
+        self.assertEqual(contact.faxes, ["+4999999999"])
+        self.assertEqual(contact.emails, ["jane@book.com"])
+        self.assertEqual(contact.email_ccs, ["jane@book.net"])
+        self.assertEqual(
+            contact.notes,
+            "I would always rather be happy than dignified. ##F, ##A, ##S",
+        )
+        self.assertEqual(contact.is_use_organization_address, True)
+        self.assertEqual(contact.address, "Thornfield Hall, main road")
+        self.assertEqual(contact.city, "Thornfield Hall")
+        self.assertEqual(contact.state, "Hallshire")
+        self.assertEqual(contact.postal_code, "01234")
+        self.assertEqual(
+            contact.birth_date, datetime.datetime.fromisoformat("1847-10-19").date()
+        )
+        self.assertEqual(contact.primary_lang, "F")
+        self.assertEqual(contact.second_lang, "A")
+        self.assertEqual(contact.third_lang, "S")
+
+        self.assertEqual(contact.registrations.count(), 1)
+        registration = contact.registrations.first()
+        self.assertEqual(registration.event, self.event)
+        self.assertEqual(registration.contact, contact)
+        self.assertEqual(registration.status, "Registered")
+        self.assertEqual(registration.priority_pass.code, "LETMEIN")
+        self.assertEqual(
+            registration.date,
+            datetime.datetime(
+                2015, 5, 8, 12, 3, 31, 19000, tzinfo=datetime.timezone.utc
+            ),
+        )
+        self.assertEqual(registration.is_funded, False)
+        self.assertEqual(
+            registration.role, RegistrationRole.objects.get(name="Alternate Head")
+        )
