6bd40a1629143c68dc9030f3c2b2a454a40f9bef
alexkiro <1538458+alexkiro@users.noreply.github.com>
2025-07-21 16:20:20 +0300

    Fix tests

--- cypress/e2e/1.admin/6.events/4.priority-passes.cy.js
+++ cypress/e2e/1.admin/6.events/4.priority-passes.cy.js
@@ -9,21 +9,12 @@ describe("Check", () => {
   });
   it("Check scan view", () => {
     cy.loginEdit();
-    visitAdmin("/admin/events/prioritypass/pass-scan-view/?code=T6UQZRYW0S");
+    cy.checkSearch({ modelName: "Priority passes", searchValue: "T6UQZRYW0S" });
+    cy.get(".field-pass_download_link a").contains("Scan").goToHref();
     cy.contains("Mr. Lyra-Pulse Solstice");
-    cy.contains("QQ:ASAS Quantum Quest: A Science Adventure Symposium");
-    cy.contains("PDAF Psychedelic Dreamscape Art Fair");
-    cy.contains("SSMF Stéllâr Sérènade Müsïc Fêstivàl");
-  });
-  it("Check scan view wrong code", () => {
-    cy.loginEdit();
-    visitAdmin("/admin/events/prioritypass/pass-scan-view/?code=XXXXXXXXXX");
-    cy.contains("Priority pass does not exist");
-  });
-  it("Check scan view no code", () => {
-    cy.loginEdit();
-    visitAdmin("/admin/events/prioritypass/pass-scan-view/");
-    cy.contains("No priority pass code provided");
+    cy.contains("Quantum Quest: A Science Adventure Symposium");
+    cy.contains("Psychedelic Dreamscape Art Fair");
+    cy.contains("Stéllâr Sérènade Müsïc Fêstivàl");
   });
   it("Check save and send email with priority pass", () => {
     cy.loginAdmin();
@@ -98,10 +89,3 @@ describe("Check", () => {
     });
   });
 });
-
-function visitAdmin(visitUrl) {
-  cy.url().then((currentUrl) => {
-    const url = new URL(currentUrl);
-    cy.visit(`${url.protocol}//${url.host}${visitUrl}`);
-  });
-}
