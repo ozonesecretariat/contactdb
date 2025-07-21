236768589995f0fce9ee8529c961b68263e0d059
alexkiro <1538458+alexkiro@users.noreply.github.com>
2025-07-21 16:30:31 +0300

    Add tests for rendering badge and pass

--- cypress/e2e/1.admin/6.events/4.priority-passes.cy.js
+++ cypress/e2e/1.admin/6.events/4.priority-passes.cy.js
@@ -16,6 +16,30 @@ describe("Check", () => {
     cy.contains("Psychedelic Dreamscape Art Fair");
     cy.contains("Stéllâr Sérènade Müsïc Fêstivàl");
   });
+  it("Check view pass as html", () => {
+    cy.loginEdit();
+    visitAdmin("/admin/events/prioritypass/372/pass/");
+    cy.contains("T6UQZRYW0S");
+    cy.contains("Guatemala");
+    cy.contains("Mr. Lyra-Pulse Solstice");
+    cy.contains("Quantum Quest: A Science Adventure Symposium");
+    cy.contains("Psychedelic Dreamscape Art Fair");
+    cy.contains("Stéllâr Sérènade Müsïc Fêstivàl");
+    cy.contains("Tashkent, Myanmar");
+    cy.contains("THIS PASS DOES NOT GRANT ACCESS TO THE VENUE.");
+  });
+  it("Check view badge as html", () => {
+    cy.loginEdit();
+    visitAdmin("/admin/events/prioritypass/372/badge/");
+    cy.contains("T6UQZRYW0S");
+    cy.contains("Lyra-Pulse");
+    cy.contains("Solstice");
+    cy.contains("Guatemala");
+    cy.contains("Government");
+    cy.contains("PDAF@2025");
+    cy.get('img[alt="https://example.com?appstore"]');
+    cy.get('img[alt="https://example.com?playstore"]');
+  });
   it("Check save and send email with priority pass", () => {
     cy.loginAdmin();
     cy.createContactGroup().then((group) => {
@@ -89,3 +113,10 @@ describe("Check", () => {
     });
   });
 });
+
+function visitAdmin(visitUrl) {
+  cy.url().then((currentUrl) => {
+    const url = new URL(currentUrl);
+    cy.visit(`${url.protocol}//${url.host}${visitUrl}`);
+  });
+}
