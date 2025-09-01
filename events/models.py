717d16ae094b006a4ba84ba52d29955d4ce55140
alexkiro <1538458+alexkiro@users.noreply.github.com>
2025-09-01 09:48:44 +0300

    Hide overflow of texts

--- events/models.py
+++ events/models.py
@@ -523,6 +523,45 @@ class PriorityPass(models.Model):
         except ValueError:
             return None
 
+    @property
+    def valid_date_range(self):
+        date_fmt = "%b %e, %Y"
+        start, end = self.valid_from, self.valid_to
+
+        if not start and not end:
+            return ""
+
+        if start == end or (bool(start) ^ bool(end)):
+            return (start or end).strftime(date_fmt)
+
+        if end < start:
+            start, end = end, start
+
+        same_year = start.year == end.year
+        same_month = start.month == end.month
+
+        if same_year and same_month:
+            return "{month} {start_day}-{end_day}, {year} ".format(
+                month=start.strftime("%b"),
+                start_day=start.day,
+                end_day=end.day,
+                year=start.year,
+            )
+
+        if same_year:
+            return "{start_month} {start_day} - {end_month} {end_day}, {year} ".format(
+                start_month=start.strftime("%b"),
+                start_day=start.day,
+                end_month=end.strftime("%b"),
+                end_day=end.day,
+                year=start.year,
+            )
+
+        return "{start_date} to {end_date}".format(
+            start_date=start.strftime(date_fmt),
+            end_date=end.strftime(date_fmt),
+        )
+
     def send_confirmation_email(self):
         if not self.main_event or not self.main_event.has_confirmation_email:
             return
