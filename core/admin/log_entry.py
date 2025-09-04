94ea85c9d162231c0a1f5f9eee08c0ed4641c8e7
alexkiro <1538458+alexkiro@users.noreply.github.com>
2025-09-04 14:37:07 +0300

    Add custom admin for log entries

---
+++ core/admin/log_entry.py
@@ -0,0 +1,25 @@
+from admin_auto_filters.filters import AutocompleteFilterFactory
+from auditlog.admin import LogEntryAdmin
+from auditlog.filters import ResourceTypeFilter
+from auditlog.models import LogEntry
+from django.contrib import admin
+from django.contrib.admin import ShowFacets
+
+admin.site.unregister(LogEntry)
+
+
+@admin.register(LogEntry)
+class CustomLogEntryAdmin(LogEntryAdmin):
+    list_per_page = 20
+    show_facets = ShowFacets.NEVER
+
+    search_fields = [
+        "object_repr",
+        "object_id",
+    ]
+
+    list_filter = [
+        "action",
+        ResourceTypeFilter,
+        AutocompleteFilterFactory("user", "actor"),
+    ]
