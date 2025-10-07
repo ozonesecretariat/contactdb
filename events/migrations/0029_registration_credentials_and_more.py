80292572b4e7b760cf3b72a7025846e31c6ad967
Andrei Meliș <andrei.melis@eaudeweb.ro>
2025-10-07 16:05:47 +0300

    Fix typo in migration name

--- events/migrations/0029_registration_credentials_and_more.py
+++ events/migrations/0029_registration_credentials_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("events", "0028_event_timezone.py"),
+        ("events", "0028_event_timezone"),
     ]
 
     operations = [
